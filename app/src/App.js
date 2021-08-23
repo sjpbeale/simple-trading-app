ba5cc4a29064dd6e858bf0806a1fc13ed589b1d8
Sam Beale <sam@easy-forex.com>
2021-08-23 05:07:00 +0300

    Switch to syled components, add routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	* {
+		margin: 0;
+		padding: 0;
+		outline: 0;
+		box-sizing: border-box;
+	}
+	body {
+		font-family: Lato,"sans-serif";
+		font-size: 14px;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		background-color: rgb(10, 16, 23);
+		color: white;
+	}
+	#root {
+		margin: 8px;
+		display: flex;
+		flex-direction: column;
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+		<GlobalStyle />
+		<Router>
+			<Route exact path="/connect">
+				<div>Connect</div>
+			</Route>
+		</Router>
+		</>
+	);
 }
 
 export default App;
